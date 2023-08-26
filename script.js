7eb2b5a4e7fcda4a7b5777e960efa1c7f9d50adb
Kiran <kiran@kali.kali>
2023-08-26 11:56:04 +0530

    Enhance UI and fix bug -> add task even input is empty

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-const todoForm=document.querySelector('.todo-form');
-const todoInput=document.querySelector('.text');
-const todoUl=document.querySelector('.todo-list');
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".input-box");
+const todoList = document.querySelector(".todo-list");
 
-todoForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-const inputValue=todoInput.value;
-const newLi=document.createElement("li");
-const liInnerHtml=`<span class="task">${inputValue}</span>
-<div class="todo-buttons">
-    <button class="done">Done</button>
-    <button class="Remove">Remove</button>`
-    newLi.innerHTML=liInnerHtml;
-    todoInput.value="";
-    todoUl.append(newLi);
-})
+  const inputValue = todoInput.value;
+  if (inputValue) {
+    const newLi = document.createElement("li");
+    const liInnerHtml = ` 
+    <span class="task">${inputValue}</span>
+    <div class="todo-buttons">
+      <i class="done fa-solid fa-check" style="color: #000000"></i>
+      <i class="Remove fa-solid fa-trash" style="color: #000000"></i>
+    </div>`;
+    newLi.innerHTML = liInnerHtml;
+    todoInput.value = "";
+    todoList.append(newLi);
+  } else {
+    alert("please Enter Your Work");
+  }
+});
 
-todoUl.addEventListener("click",(e)=>{
-   if(e.target.classList.contains("Remove")){
-    const targetSpan=e.target.parentNode.parentNode;
-    targetSpan.remove();
-   }
-
-   if(e.target.classList.contains("done")){
-     const liSpan=e.target.parentNode.previousElementSibling;
-     liSpan.style.textDecoration="line-through";  
-   }
-     
-})
+todoList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("Remove")) {
+    e.target.parentElement.parentElement.remove();
+  }
+  if (e.target.classList.contains("task")) {
+    e.target.setAttribute("contenteditable", "true");
+  }
+  if (e.target.classList.contains("done")) {
+    e.target.parentElement.previousElementSibling.style.textDecoration =
+      "line-through";
+  }
+});
